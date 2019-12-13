cc511ab4268c8b33463ce5c62202b3ae15c534c5
Michael Sabzevari <michaelsabzevari@gmail.com>
2019-12-13 13:24:57 -0500

    too many struggles, must come back later

--- src/App.js
+++ src/App.js
@@ -1,47 +1,79 @@
 import React from 'react';
 // import axios from 'axios';
 import './App.css';
+import Lyrics from './components/Lyrics';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      // artist: [],
-      // title: [],
-      items: {},
-      isLoaded: false
+      artist: "",
+      title: "",
+      lyrics: "", 
+      displayLyrics: [],
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleTitleChange = this.handleTitleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
-
-
   componentDidMount() {
-    fetch('https://api.lyrics.ovh/v1/maroon 5/this love')
+    fetch(`https://api.lyrics.ovh/v1/${this.state.artist}/${this.state.title}`)
       .then(res => res.json())
       .then(json => {
         this.setState({
-          isLoaded: true,
-          items: json,
+          lyrics: json,
         })
+        this.setState({ displayLyrics: this.state.lyrics });
       })
-  }
-
-  render() {
-    const { items, isLoaded } = this.state
-    if (!isLoaded) {
-      return <div>Loading...</div>
-    } else {
-      return (
-        <div className="App">
-          <ul>
-            {items.map(item => (
-              <div>
-                <h1>{item.lyrics}</h1>
-              </div>
-            ))}
-          </ul>
-        </div>
-      )
     }
+
+    handleChange(event) {
+      this.setState({artist: event.target.value})
+    }
+    handleTitleChange(event) {
+      this.setState({title: event.target.value})
+    }
+    handleSubmit(event) {
+      this.setState({displayLyrics: this.state.lyrics})
+      event.preventDefault()
+    }
+    
+    renderNames() {
+    const { artist, title } = this.state
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Artist:
+          <input type="text" value={artist} onChange={this.handleChange} />
+        </label>
+        <label>
+          Title:
+          <input type="text" value={title} onChange={this.handleTitleChange} />
+        </label>
+          <input type="submit" value="Submit" />
+      </form>
+    )
+  }
+  renderSongs = () => {
+    return this.state.displayLyrics.map((lyric, index) => {
+      const { lyrics } = lyric;
+      return (
+        <Lyrics
+        key={index}
+        lyrics={lyrics}
+        ></Lyrics>
+      )
+    })
+  }
+  render() {
+    console.log(this.state.lyrics)
+    console.log(this.state.displayLyrics)
+    return (
+      <div>
+        <div>{this.renderSongs()}</div>
+        <div>{this.renderNames()}</div>
+      </div>
+    )
   }
 }
 export default App;
---
+++ src/components/Lyrics/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Lyrics = (props) => {
+    return (
+        <div>
+            <h2>{props.lyrics}</h2>
+        </div>
+    )
+}
+export default Lyrics
