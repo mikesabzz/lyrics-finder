ce27bfbe8b3930b83ce91d8fad2dc97b94773bae
mikesabzz <michaelsabzevari@gmail.com>
2023-08-02 15:35:11 -0400

    backend deployed to netlify

--- server.js
+++ server.js
@@ -39,5 +39,6 @@ app.get('/lyrics', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  // console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Backend URL: ${process.env.NODE_ENV}`);
 });
