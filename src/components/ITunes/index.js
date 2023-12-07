f20080df8f7ea01176130f2f4ede67220fb07786
mikesabzz <michaelsabzevari@gmail.com>
2023-12-07 13:13:50 -0500

    changed itunes play to button and fetch the correct data

--- src/components/ITunes/index.js
+++ src/components/ITunes/index.js
@@ -1,28 +1,49 @@
-import React from "react";
-import './ITunes.css'
+import React, { useRef } from "react";
+import "./ITunes.css";
 
 const ITunes = (props) => {
-    const array = props.itunes.map((itune, index) => {
-        if (props.artist !== itune.artistName.toLowerCase()) {
-            return null;
-        } else {
-        return (
-            <div className="Itunes-url" key={index}>
-                <img src={itune.artworkUrl100} alt="Album Art" />
-                <br />
-                <audio controls>
-                    <source src={itune.previewUrl} />
-                </audio>
-            </div>
-        )}})
-    const ituneAudio = props.loading ? <div>Loading Preview...</div> : array
+  const audioRef = useRef(null);
 
-    return (
-        <div className="lyric-body">
-            <div>{ituneAudio}</div>
+  const playPreview = (previewUrl) => {
+    if (audioRef.current) {
+      // Pause and reset the previous audio element
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+    }
+
+    // Create a new audio element
+    const audio = new Audio(previewUrl);
+    audio.play();
+
+    // Save the new audio element to the ref for future use
+    audioRef.current = audio;
+  };
+  const array = props.itunes.map((itune, index) => {
+    if (props.artist !== itune.artistName.toLowerCase()) {
+      return null;
+    } else {
+      return (
+        <div className="Itunes-url" key={index}>
+          <img src={itune.artworkUrl100} alt="Album Art" />
+          <br />
+
+          <button
+            className="btn btn-light mt-2"
+            onClick={() => playPreview(itune.previewUrl)}
+          >
+            Play Preview
+          </button>
         </div>
-    )
+      );
+    }
+  });
+  const ituneAudio = props.loading ? <div>Loading Preview...</div> : array;
 
-}     
+  return (
+    <div className="lyric-body">
+      <div>{ituneAudio}</div>
+    </div>
+  );
+};
 
-export default ITunes
+export default ITunes;
